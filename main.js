3842726efc35bf884d85be64acb8f4dc2b2c75b0
iuyyuiokyuuiu <138327609+iuyyuiokyuuiu@users.noreply.github.com>
2023-07-21 16:52:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 objects = [];
 status = "";
 
-function preload(){
-  video = createVideo('video.mp4');
-}
+
 
 
 function setup() {
   canvas = createCanvas(480, 380);
   canvas.center();
+  video = createCapture(VIDEO);
+  video.size(480, 380)
   video.hide();
 }
 
@@ -17,14 +17,13 @@ function start()
 {
   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
   document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+  objectName = document.getElementById("objectName").value
 }
 
 function modelLoaded() {
   console.log("Modelo Carregado!")
   status = true;
-  video.loop();
-  video.speed(1);
-  video.volume(0);
+  
 }
 
 function gotResult(error, results) {
